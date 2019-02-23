52fed179097c29fa24130ad51ed7a9e8170a78ec
LinkTed <LinkTed@users.noreply.github.com>
2019-02-23 20:17:01 +0100

    Updated dependencies

--- src/main.rs
+++ src/main.rs
@@ -23,7 +23,7 @@ static LOGGER: Logger = Logger {};
 
 fn main() {
     let matches = App::new("DNS over HTTPS client")
-        .version("1.3.5")
+        .version("1.3.6")
         .author("link.ted@mailbox.org")
         .about("Open a local UDP (DNS) port and forward DNS queries to a remote HTTP/2.0 server.\nBy default, the client will connect to the Cloudflare DNS service.")
         .arg(Arg::with_name("listen-addr")
