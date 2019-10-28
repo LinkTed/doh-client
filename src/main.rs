11dd2a253188e95b8ec14fca526432260fde468b
LinkTed <LinkTed@users.noreply.github.com>
2019-10-28 17:04:27 +0100

    Move the clap::App creation to a dedicated function

--- src/main.rs
+++ src/main.rs
@@ -7,100 +7,19 @@ extern crate doh_client;
 
 use log::{set_max_level, set_logger, LevelFilter};
 
-use clap::{Arg, App};
-
 use doh_client::{Config, run, UdpListenSocket, Logger};
 
 use std::net::SocketAddr;
 use std::process::exit;
+
+use doh_client::get_app;
 
 
 static LOGGER: Logger = Logger {};
 
 
 fn main() {
-    let matches = App::new("DNS over HTTPS client")
-        .version("1.4.5")
-        .author("link.ted@mailbox.org")
-        .about("Open a local UDP (DNS) port and forward DNS queries to a remote HTTP/2.0 server.\nBy default, the client will connect to the Cloudflare DNS service.")
-        .arg(Arg::with_name("listen-addr")
-            .short("l")
-            .long("listen-addr")
-            .conflicts_with("listen-activation")
-            .takes_value(true)
-            .value_name("Addr")
-            .help("Listen address [default: 127.0.0.1:53]")
-            .required(false))
-        .arg(Arg::with_name("listen-activation")
-            .long("listen-activation")
-            .help("Use file descriptor 3 under Unix as UDP socket or launch_activate_socket() under Mac OS")
-            .required(false))
-        .arg(Arg::with_name("remote-addr")
-            .short("r")
-            .long("remote-addr")
-            .takes_value(true)
-            .value_name("Addr")
-            .help("Remote address")
-            .default_value("1.1.1.1:443")
-            .required(false))
-        .arg(Arg::with_name("domain")
-            .short("d")
-            .long("domain")
-            .takes_value(true)
-            .value_name("Domain")
-            .help("The domain name of the remote server")
-            .default_value("cloudflare-dns.com")
-            .required(false))
-        .arg(Arg::with_name("retries")
-            .takes_value(true)
-            .long("retries")
-            .value_name("UNSIGNED INT")
-            .help("The number of retries to connect to the remote server")
-            .default_value("3")
-            .required(false))
-        .arg(Arg::with_name("timeout")
-            .takes_value(true)
-            .short("t")
-            .long("timeout")
-            .value_name("UNSIGNED LONG")
-            .help("The time in seconds after that the connection would be closed if no response is received from the server")
-            .default_value("2")
-            .required(false))
-        .arg(Arg::with_name("cafile")
-            .takes_value(true)
-            .value_name("CAFILE")
-            .help("The path to the pem file, which contains the trusted CA certificates")
-            .required(true))
-        .arg(Arg::with_name("path")
-            .short("p")
-            .long("path")
-            .takes_value(true)
-            .value_name("STRING")
-            .help("The path of the URI")
-            .default_value("dns-query")
-            .required(false))
-        .arg(Arg::with_name("v")
-            .short("v")
-            .multiple(true)
-            .help("Sets the level of verbosity"))
-        .arg(Arg::with_name("get")
-            .short("g")
-            .long("get")
-            .help("Use the GET method for the HTTP/2.0 request")
-            .required(false))
-        .arg(Arg::with_name("cache-size")
-            .long("cache-size")
-            .short("c")
-            .takes_value(true)
-            .value_name("UNSIGNED LONG")
-            .help("The size of the private HTTP cache\nIf the size is 0 then the private HTTP cache is not used (ignores cache-control)")
-            .default_value("1024")
-            .required(false))
-        .arg(Arg::with_name("cache-fallback")
-            .long("cache-fallback")
-            .help("Use expired cache entries if no response is received from the server")
-            .required(false))
-        .get_matches();
+    let matches = get_app().get_matches();
 
     if let Err(e) = set_logger(&LOGGER) {
         eprintln!("Could not set logger: {}", e);
