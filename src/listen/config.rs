LinkTed/doh-client

commit ae43cff90c64873041ba1d6eb38927482f462a54

Refactored the code and add socks5 proxy feature

src/listen/config.rs
@@ -1,0 +1,97 @@
+use std::fmt::{Display, Formatter, Result as FmtResult};
+use std::io::Result as IoResult;
+use std::net::SocketAddr;
+use std::net::UdpSocket as StdUdpSocket;
+
+use tokio::net::UdpSocket as TokioUdpSocket;
+
+#[cfg(target_os = "macos")]
+use libc::size_t;
+#[cfg(target_os = "macos")]
+use std::os::raw::{c_char, c_int, c_void};
+
+#[cfg(target_os = "macos")]
+extern "C" {
+    fn launch_activate_socket(name: *const c_char, fds: *mut *mut c_int, cnt: *mut size_t)
+        -> c_int;
+}
+
+#[cfg(target_os = "macos")]
+fn get_activation_socket() -> IoResult<StdUdpSocket> {
+    use libc::free;
+    use std::ffi::CString;
+    use std::io::ErrorKind::Other;
+    use std::os::unix::io::FromRawFd;
+    use std::ptr::null_mut;
+    unsafe {
+        let mut fds: *mut c_int = null_mut();
+        let mut cnt: size_t = 0;
+
+        let name = CString::new("Listeners").expect("CString::new failed");
+        if launch_activate_socket(name.as_ptr(), &mut fds, &mut cnt) == 0 {
+            if cnt == 1 {
+                let socket = net::UdpSocket::from_raw_fd(*fds.offset(0));
+                free(fds as *mut c_void);
+                Ok(socket)
+            } else {
+                Err(io::Error::new(Other, "Could not get fd: cnt != 1"))
+            }
+        } else {
+            Err(io::Error::new(
+                Other,
+                "Could not get fd: launch_activate_socket != 0",
+            ))
+        }
+    }
+}
+
+#[cfg(all(target_family = "unix", not(target_os = "macos")))]
+fn get_activation_socket() -> IoResult<StdUdpSocket> {
+    use std::os::unix::io::FromRawFd;
+    unsafe { Ok(StdUdpSocket::from_raw_fd(3)) }
+}
+
+#[cfg(target_family = "windows")]
+fn get_activation_socket() -> IoResult<StdUdpSocket> {
+    use std::io::ErrorKind::Other;
+    Err(io::Error::new(
+        Other,
+        "This is not supported in windows platforms",
+    ))
+}
+
+#[derive(Copy, Clone)]
+pub enum Config {
+    Addr(SocketAddr),
+    Activation,
+}
+
+impl Config {
+    pub(crate) async fn into_socket(self) -> IoResult<TokioUdpSocket> {
+        let socket = match self {
+            Config::Addr(socket_addr) => TokioUdpSocket::bind(&socket_addr).await?,
+            Config::Activation => {
+                let socket = get_activation_socket()?;
+                TokioUdpSocket::from_std(socket)?
+            }
+        };
+        Ok(socket)
+    }
+}
+
+impl Display for Config {
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
+        match self {
+            Config::Addr(socket_addr) => write!(f, "{}", socket_addr),
+            Config::Activation => {
+                if cfg!(target_os = "macos") {
+                    write!(f, "file descriptor of launch_activate_socket()")
+                } else if cfg!(target_family = "unix") {
+                    write!(f, "file descriptor 3")
+                } else {
+                    write!(f, "this is not supported")
+                }
+            }
+        }
+    }
+}

src/config.rs
@@ -1,0 +1,100 @@
+use crate::listen::{handler as listen_handler, Config as ListenConfig};
+use crate::remote::{Host as RemoteHost, Session as RemoteSession};
+use crate::{Cache, Context, DohError, DohResult};
+
+use futures::lock::Mutex;
+
+use tokio::net::udp::RecvHalf;
+
+use rustls::ClientConfig;
+
+use std::fs::File;
+use std::io::BufReader;
+use std::io::Result as IoResult;
+use std::sync::Arc;
+
+fn create_client_config(cafile: &str) -> DohResult<ClientConfig> {
+    let certfile = File::open(&cafile)?;
+    let mut config = ClientConfig::new();
+    if let Err(()) = config
+        .root_store
+        .add_pem_file(&mut BufReader::new(certfile))
+    {
+        return Err(DohError::PEMParser);
+    }
+    config.alpn_protocols.push(vec![104, 50]); // h2
+    Ok(config)
+}
+
+/// The configuration object for the `doh-client`.
+pub struct Config {
+    listen_config: ListenConfig,
+    remote_host: RemoteHost,
+    domain: String,
+    client_config: Arc<ClientConfig>,
+    uri: String,
+    retries: u32,
+    timeout: u64,
+    post: bool,
+    cache_size: usize,
+    cache_fallback: bool,
+}
+
+impl Config {
+    /// Create a new `doh_client::Config` object.
+    pub fn new(
+        listen_config: ListenConfig,
+        remote_host: RemoteHost,
+        domain: &str,
+        cafile: &str,
+        path: &str,
+        retries: u32,
+        timeout: u64,
+        post: bool,
+        cache_size: usize,
+        cache_fallback: bool,
+    ) -> DohResult<Config> {
+        let client_config = create_client_config(&cafile)?;
+
+        let uri = format!("https://{}/{}", domain, path);
+
+        if cache_fallback && cache_size == 0 {
+            return Err(DohError::CacheSize);
+        }
+
+        Ok(Config {
+            listen_config,
+            remote_host,
+            domain: domain.to_string(),
+            client_config: Arc::new(client_config),
+            uri,
+            retries,
+            timeout,
+            post,
+            cache_size,
+            cache_fallback,
+        })
+    }
+
+    pub(crate) async fn into(self) -> IoResult<(RecvHalf, Context)> {
+        let cache = if self.cache_size == 0 {
+            None
+        } else {
+            Some(Mutex::new(Cache::new(self.cache_size)))
+        };
+        let cache_fallback = self.cache_fallback;
+        let timeout = self.timeout;
+        let socket = self.listen_config.into_socket().await?;
+        let (recv, sender) = listen_handler(socket);
+        let remote_session = RemoteSession::new(
+            self.remote_host,
+            self.domain,
+            self.client_config,
+            self.uri,
+            self.retries,
+            self.post,
+        );
+        let context = Context::new(cache, cache_fallback, timeout, remote_session, sender);
+        Ok((recv, context))
+    }
+}

src/remote/config.rs
@@ -1,0 +1,29 @@
+use std::sync::Arc;
+
+use rustls::ClientConfig;
+
+pub(super) struct Config {
+    pub(super) domain: String,
+    pub(super) client_config: Arc<ClientConfig>,
+    pub(super) uri: String,
+    pub(super) retries: u32,
+    pub(super) post: bool,
+}
+
+impl Config {
+    pub(super) fn new(
+        domain: String,
+        client_config: Arc<ClientConfig>,
+        uri: String,
+        retries: u32,
+        post: bool,
+    ) -> Config {
+        Config {
+            domain,
+            client_config,
+            uri,
+            retries,
+            post,
+        }
+    }
+}

.clippy.toml
@@ -1,0 +1,2 @@
+cognitive-complexity-threshold = 10 
+too-many-arguments-threshold = 10

.travis.yml
@@ -5,6 +5,7 @@ rust:
 before_script:
   - rustup component add rustfmt
 script:
+  - cargo build --no-default-features --all --verbose
   - cargo build --verbose --all
   - cargo test --verbose --all
   - cargo fmt -- --check

Cargo.toml
@@ -1,6 +1,6 @@
 [package]
 name = "doh-client"
-version = "2.1.2"
+version = "2.2.0"
 edition = "2018"
 authors = ["LinkTed <link.ted@mailbox.org>"] 
 license = "BSD-3-Clause"
@@ -25,23 +25,36 @@ categories = [
 [badges]
 travis-ci = { repository = "LinkTed/doh-client" }
 
+[features]
+default = ["socks5"]
+socks5 = ["url", "tokio-socks"]
+
 [dependencies]
-libc = "0.2.68"
-data-encoding = "2.1.2"
+libc = "0.2.69"
+base64 = "0.12.1"
 log = "0.4.8"
 env_logger = "0.7.1"
 clap = "2.33.0"
 tokio-rustls = "0.12.2"
-futures = "0.3.4"
-h2 = "0.2.4"
+futures = "0.3.5"
+h2 = "0.2.5"
 http = "0.2.1" 
 rustls = "0.16.0"
 webpki = "0.21.2"
 bytes = "0.5.4"
 lru = "0.4.3"
+dns-message-parser = "0.2.0"
+
+[dependencies.url]
+version = "2.1.1"
+optional = true
+
+[dependencies.tokio-socks]
+version = "0.2.2"
+optional = true
 
 [dependencies.tokio]
-version = "0.2.14"
+version = "0.2.20"
 features = ["rt-threaded", "net", "time", "macros"]
 
 [dependencies.tokio-util]

PKGBUILD
@@ -2,10 +2,10 @@
 # Maintainer: LinkTed <link.ted@mailbox.org>
 
 pkgname=doh-client
-pkgver=2.1.2
+pkgver=2.2.0
 pkgrel=1
 pkgdesc="doh-client is a DNS over HTTPS client"
-arch=("x86_64")
+arch=("x86_64" "armv7h")
 url="https://github.com/LinkTed/doh-client"
 license=("BSD 3-Clause")
 depends=("ca-certificates-utils")

completions/_doh-client
@@ -17,8 +17,8 @@ _doh-client() {
     _arguments "${_arguments_options[@]}" \
 '(--listen-activation)-l+[Listen address \[default: 127.0.0.1:53\]]' \
 '(--listen-activation)--listen-addr=[Listen address \[default: 127.0.0.1:53\]]' \
-'-r+[Remote address]' \
-'--remote-addr=[Remote address]' \
+'-r+[Remote address/hostname to the DOH server (If a hostname is used then another DNS server has to be configured)]' \
+'--remote-host=[Remote address/hostname to the DOH server (If a hostname is used then another DNS server has to be configured)]' \
 '-d+[The domain name of the remote server]' \
 '--domain=[The domain name of the remote server]' \
 '--retries=[The number of retries to connect to the remote server]' \
@@ -30,6 +30,8 @@ _doh-client() {
 If the size is 0 then the private HTTP cache is not used (ignores cache-control)]' \
 '--cache-size=[The size of the private HTTP cache
 If the size is 0 then the private HTTP cache is not used (ignores cache-control)]' \
+'--socks5=[Socks5 proxy URL
+(example: socks5://user:password@example.com or socks5h://example.com)]' \
 '--listen-activation[Use file descriptor 3 under Unix as UDP socket or launch_activate_socket() under Mac OS]' \
 '-g[Use the GET method for the HTTP/2.0 request]' \
 '--get[Use the GET method for the HTTP/2.0 request]' \

commit bfd53ea443aeb5e603cb1a37c8b61eb3e2cc3b5b

fix build on macos (and windows)
This fixes the build on macos that broke in ae43cff because the new
file does not `use std::io`.
It should also fix the build on windows which seems to have the same
issue.

src/listen/config.rs
@@ -20,6 +20,7 @@ extern "C" {
 fn get_activation_socket() -> IoResult<StdUdpSocket> {
     use libc::free;
     use std::ffi::CString;
+    use std::io;
     use std::io::ErrorKind::Other;
     use std::os::unix::io::FromRawFd;
     use std::ptr::null_mut;
@@ -30,7 +31,7 @@ fn get_activation_socket() -> IoResult<StdUdpSocket> {
         let name = CString::new("Listeners").expect("CString::new failed");
         if launch_activate_socket(name.as_ptr(), &mut fds, &mut cnt) == 0 {
             if cnt == 1 {
-                let socket = net::UdpSocket::from_raw_fd(*fds.offset(0));
+                let socket = StdUdpSocket::from_raw_fd(*fds.offset(0));
                 free(fds as *mut c_void);
                 Ok(socket)
             } else {
@@ -53,6 +54,7 @@ fn get_activation_socket() -> IoResult<StdUdpSocket> {
 
 #[cfg(target_family = "windows")]
 fn get_activation_socket() -> IoResult<StdUdpSocket> {
+    use std::io;
     use std::io::ErrorKind::Other;
     Err(io::Error::new(
         Other,